fdfca2b7552827dcd2f2642c5fdb18bb42cdf3f8
Isaac Ortiz <isaac.ortiz@televisatim.com>
2019-04-30 13:42:00 -0500

    Widget

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -9,25 +9,78 @@ const Calendar = () => (
                     Título
                 </h2>
             </div>
-            <div className="Calendar__EventContainer WhiteLabel">
-                <div className="Calendar__EventDateContainer Whitelabel">
-                    <h2 className="Calendar__EventDateMonth Whitelabel">
-                        JUL
-                    </h2>
-                    <h2 className="Calendar__EventDateDay Whitelabel">
-                        30
-                    </h2>
+            <div className="Calendar__EventsContainer WhiteLabel">
+                <div className="Calendar__EventContainer WhiteLabel">
+                    <div className="Calendar__EventDateContainer WhiteLabel">
+                        <h2 className="Calendar__EventDateDay WhiteLabel">
+                            Sábado
+                        </h2>
+                        <h2 className="Calendar__EventDateNumber WhiteLabel">
+                            16
+                        </h2>
+                        <h2 className="Calendar__EventDateMonth WhiteLabel">
+                            NOV
+                        </h2>
+                    </div>
+                    <div className="Calendar__EventDescriptionContainer WhiteLabel">
+                        <Link to="/" className="Calendar__EventDescriptionLink WhiteLabel">
+                            Corona Capital 2019
+                        </Link>
+                        <p className="Calendar__EventDescriptionTime WhiteLabel">
+                            14:00 - 00:30
+                        </p>
+                        <Link to="/" className="Calendar__EventAdressLink WhiteLabel">
+                            Curva 4, autódromo hermanos rodríguez, CDMX, México
+                        </Link>
+                    </div>
                 </div>
-                <div className="Calendar__EventDescriptionContainer Whitelabel">
-                    <Link to="/" className="CardPerson__EventDescriptionLink WhiteLabel">
-                        Ver Más
-                    </Link>
-                    <p className="Calendar__DescriptionTime WhiteLabel">
-                        20:30
-                    </p>
-                    <Link to="/" className="CardPerson__EventAdressLink WhiteLabel">
-                        CDMX, México
-                    </Link>
+                <div className="Calendar__EventContainer WhiteLabel">
+                    <div className="Calendar__EventDateContainer WhiteLabel">
+                        <h2 className="Calendar__EventDateDay WhiteLabel">
+                            Domingo
+                        </h2>
+                        <h2 className="Calendar__EventDateNumber WhiteLabel">
+                            17
+                        </h2>
+                        <h2 className="Calendar__EventDateMonth WhiteLabel">
+                            NOV
+                        </h2>
+                    </div>
+                    <div className="Calendar__EventDescriptionContainer WhiteLabel">
+                        <Link to="/" className="Calendar__EventDescriptionLink WhiteLabel">
+                            Corona Capital 2019
+                        </Link>
+                        <p className="Calendar__EventDescriptionTime WhiteLabel">
+                            14:00 - 00:30
+                        </p>
+                        <Link to="/" className="Calendar__EventAdressLink WhiteLabel">
+                            Curva 4, autódromo hermanos rodríguez, CDMX, México
+                        </Link>
+                    </div>
+                </div>
+                <div className="Calendar__EventContainer WhiteLabel">
+                    <div className="Calendar__EventDateContainer WhiteLabel">
+                        <h2 className="Calendar__EventDateDay WhiteLabel">
+                            Lunes
+                        </h2>
+                        <h2 className="Calendar__EventDateNumber WhiteLabel">
+                            18
+                        </h2>
+                        <h2 className="Calendar__EventDateMonth WhiteLabel">
+                            NOV
+                        </h2>
+                    </div>
+                    <div className="Calendar__EventDescriptionContainer WhiteLabel">
+                        <Link to="/" className="Calendar__EventDescriptionLink WhiteLabel">
+                            Aniversario de la Revolución Mexicana
+                        </Link>
+                        <p className="Calendar__EventDescriptionTime WhiteLabel">
+                            14:00 - 00:30
+                        </p>
+                        <Link to="/" className="Calendar__EventAdressLink WhiteLabel">
+                            Ángel de la Independencia, CDMX, México
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
