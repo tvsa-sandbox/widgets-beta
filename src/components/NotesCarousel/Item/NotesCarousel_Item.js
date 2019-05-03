5aae1daf4ae3122edcaa92ec632afd0a6b8c854c
Edgar Villeda <edgar.villeda@televisatim.com>
2019-05-03 16:44:06 -0500

    FIX: NotesCarousel Se agrega alt a la imagen

--- src/components/NotesCarousel/Item/NotesCarousel_Item.js
+++ src/components/NotesCarousel/Item/NotesCarousel_Item.js
@@ -8,7 +8,7 @@ const NotesCarousel_Item = (props) => (
 
           <div className="NotesCarousel__ItemThumbnail WhiteLabel">
               <div className="NotesCarousel__ItemImageContainer WhiteLabel">
-                  <img src={withPrefix("/static/aspect-ratio-images/image2.jpg")} className="NotesCarousel__ItemImage WhiteLabel" />
+                  <img src={withPrefix("/static/aspect-ratio-images/image2.jpg")} className="NotesCarousel__ItemImage WhiteLabel" alt="Image title"/>
               </div>
           </div>
 
