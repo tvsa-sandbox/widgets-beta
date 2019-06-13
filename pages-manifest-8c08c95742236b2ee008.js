1ae98bd23bebb99b0cc19bc10898d044a074f8e6
jose morales <jose.morales@televisatim.com>
2019-06-13 17:50:28 -0500

    Updates

---
+++ pages-manifest-8c08c95742236b2ee008.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{142:function(e){e.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"829/path---dev-404-page-5-f-9-fab-o7af702RpOxvpRo1x1JR8NttFI",index:"140/path---index-6a9-0SUcWyAf8ecbYDsMhQkEfPzV8","sq--src-layouts-index-js":755544856}}}}]);
+//# sourceMappingURL=pages-manifest-8c08c95742236b2ee008.js.map
