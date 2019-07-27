1c5ff0702eca04bcfa27c77f5a2b6d12a7f16371
MaFerMarquez <maria.martinez@televisatim.com>
2019-07-26 19:06:07 -0500

    Fix: Hamburger

--- src/components/MenuHamburger/MenuHamburger.js
+++ src/components/MenuHamburger/MenuHamburger.js
@@ -5,9 +5,9 @@ const MenuHamburger = () => (
 	<div className="MenuHamburger WhiteLabel">
 	  <input type="checkbox" href="#" className="MenuHamburger-Close WhiteLabel" name="MenuHamburger-Close" id="MenuHamburger-Close"/>
 	  <label className="MenuHamburger-Close-button WhiteLabel" for="MenuHamburger-Close">
-	    <span className="MenuHamburger MenuHamburger-1 WhiteLabel"></span>
-	    <span className="MenuHamburger MenuHamburger-2 WhiteLabel"></span>
-	    <span className="MenuHamburger MenuHamburger-3 WhiteLabel"></span>
+	    <span className="MenuHamburger__Line MenuHamburger-1 WhiteLabel"></span>
+	    <span className="MenuHamburger__Line MenuHamburger-2 WhiteLabel"></span>
+	    <span className="MenuHamburger__Line MenuHamburger-3 WhiteLabel"></span>
 	  </label>
 	</div>
 
