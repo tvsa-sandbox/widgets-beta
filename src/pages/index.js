66f48d568b1b32928cdf79a18e2699e8821b2381
jose morales <jose.morales@televisatim.com>
2019-07-30 12:00:14 -0500

    ADD: new index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import Layout from "../layouts";
 
 // Components
 
-import Positions from "../components/Positions/Positions";
-import Versus from "../components/Versus/Versus";
 import SuperCard from "../components/SuperCard/SuperCard";
 import Related1 from "../components/Related1/";
 import Related2 from "../components/Related2/";
@@ -18,8 +16,6 @@ import NotesCarousel from "../components/NotesCarousel/";
 const IndexPage = () => (
     <Layout>
 
-        <Positions />
-        <Versus />
         <SuperCard />
         <Related1 />
         <Related2 />
