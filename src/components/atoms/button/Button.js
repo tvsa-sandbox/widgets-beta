add98449866e75f330696ba1c870423a4c5db0dc
jose morales <jose.morales@televisatim.com>
2019-09-10 19:44:05 -0500

    ADD: Button markup test

---
+++ src/components/atoms/button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => (
+    <div className="BtnContainer WhiteLabel">
+        <button className="Btn WhiteLabel">
+            texto x
+        </button>
+    </div>
+);
+
+export default Button;
---
+++ src/pages/atoms.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../layouts";
+//import { withPrefix } from "gatsby";
+
+// Atoms
+
+import Button from '../components/atoms/button';
+
+const IndexPage = () => (
+    <Layout>
+        <Button />
+    </Layout>
+);
+
+export default IndexPage;
