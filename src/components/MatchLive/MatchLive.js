6a3d970edb16a168e815a8a4d7c7c8492f955e54
Karen Torres <karen.torres@televisatim.com>
2019-09-18 19:17:40 -0500

    Tudn widgets

---
+++ src/components/MatchLive/MatchLive.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const MatchLive = () => (
+    <section className="MatchLive WhiteLabel">
+        <div className="MatchLive__ButtonContainer WhiteLabel">
+            <button className="MatchLive__MoreButton-Inactive WhiteLabel">
+                <div className="Display">
+                    Más Resultados
+                </div>
+            </button>
+        </div>
+        <div className="MatchLive__Container WhiteLabel">
+            <div className="MatchLive__Selector WhiteLabel">
+                <div className="MatchLive__SelectorLeague WhiteLabel">
+                    <div className="Display">
+                        Combo 
+                    </div>
+                </div>
+            </div>
+            <div className="MatchLive__Wrapper WhiteLabel">
+                <div className="MatchLive__WrapperTeams WhiteLabel">
+                    <div className="MatchLive__WrapperBackground WhiteLabel">
+                        <div className="Display">
+                            Wrapper 
+                        </div>
+                    </div>
+                </div>
+                <div className="MatchLive__WrapperTeams WhiteLabel">
+                    <div className="MatchLive__WrapperBackground WhiteLabel">
+                        <div className="Display">
+                            Wrapper 
+                        </div>
+                    </div>
+                </div>
+                <div className="MatchLive__WrapperTeams WhiteLabel">
+                    <div className="MatchLive__WrapperBackground WhiteLabel">
+                        <div className="Display">
+                            Wrapper 
+                        </div>
+                    </div>
+                </div>
+                <div className="MatchLive__WrapperTeams WhiteLabel">
+                    <div className="MatchLive__WrapperBackground WhiteLabel">
+                        <div className="Display">
+                            Wrapper 
+                        </div>
+                    </div>
+                </div>
+                <div className="MatchLive__WrapperTeams WhiteLabel">
+                    <div className="MatchLive__WrapperBackground WhiteLabel">
+                        <div className="Display">
+                            Wrapper 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+);
+
+export default MatchLive;
