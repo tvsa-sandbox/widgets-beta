df6410499163715491af49015d7b547c6fca2813
Karen Torres <karen.torres@televisatim.com>
2019-09-27 16:36:29 -0500

    ADD: EventList

---
+++ src/components/EventList/EventList.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Link } from "gatsby";
+import { withPrefix } from "gatsby";
+
+const EventList = () => (
+    <section className="EventList WhiteLabel">
+        <div className="EventList__BoxContainer WhiteLabel">
+            <div className="EventList__TitleContainer WhiteLabel">
+                <h2 className="EventList__Title WhiteLabel">
+                    Las Recientes
+                </h2>
+            </div>
+            <div className="EventList__BlockContainer WhiteLabel">
+                <div className="EventList__TimeContainer WhiteLabel">
+                    <span className="EventList__Time WhiteLabel">
+                        12:00
+                    </span>
+                </div>
+                <div className="EventList__HeadlinesContainer WhiteLabel">
+                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                        <h3 className="EventList__HeadlinesText WhiteLabel">
+                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                        </h3>
+                    </Link>
+                </div>
+            </div>
+            <div className="EventList__BlockContainer WhiteLabel">
+                <div className="EventList__TimeContainer WhiteLabel">
+                    <span className="EventList__Time WhiteLabel">
+                        12:00
+                    </span>
+                </div>
+                <div className="EventList__HeadlinesContainer WhiteLabel">
+                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                        <h3 className="EventList__HeadlinesText WhiteLabel">
+                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                        </h3>
+                    </Link>
+                </div>
+            </div>
+            <div className="EventList__BlockContainer WhiteLabel">
+                <div className="EventList__TimeContainer WhiteLabel">
+                    <span className="EventList__Time WhiteLabel">
+                        12:00
+                    </span>
+                </div>
+                <div className="EventList__HeadlinesContainer WhiteLabel">
+                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                        <h3 className="EventList__HeadlinesText WhiteLabel">
+                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                        </h3>
+                    </Link>
+                </div>
+            </div>
+            <div className="EventList__BlockContainer WhiteLabel">
+                <div className="EventList__TimeContainer WhiteLabel">
+                    <span className="EventList__Time WhiteLabel">
+                        12:00
+                    </span>
+                </div>
+                <div className="EventList__HeadlinesContainer WhiteLabel">
+                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                        <h3 className="EventList__HeadlinesText WhiteLabel">
+                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                        </h3>
+                    </Link>
+                </div>
+            </div>
+            <div className="EventList__BlockContainer WhiteLabel">
+                <div className="EventList__TimeContainer WhiteLabel">
+                    <span className="EventList__Time WhiteLabel">
+                        12:00
+                    </span>
+                </div>
+                <div className="EventList__HeadlinesContainer WhiteLabel">
+                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                        <h3 className="EventList__HeadlinesText WhiteLabel">
+                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                        </h3>
+                    </Link>
+                </div>
+            </div>
+            <div className="EventList__BlockContainer WhiteLabel">
+                <div className="EventList__TimeContainer WhiteLabel">
+                    <span className="EventList__Time WhiteLabel">
+                        12:00
+                    </span>
+                </div>
+                <div className="EventList__HeadlinesContainer WhiteLabel">
+                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                        <h3 className="EventList__HeadlinesText WhiteLabel">
+                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                        </h3>
+                    </Link>
+                </div>
+            </div>
+            <div className="EventList__MoreContainer WhiteLabel">
+                <Link to="/" className="EventList__More WhiteLabel">
+                    Ver Más
+                </Link>
+            </div>
+        </div>
+    </section>
+);
+
+export default EventList;
