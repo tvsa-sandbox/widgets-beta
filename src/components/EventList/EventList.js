0abc1fb5698650b32796f39f840c3daa02f3c760
Karen Torres <karen.torres@televisatim.com>
2019-09-30 20:10:34 -0500

    FIX: EventList Styles

--- src/components/EventList/EventList.js
+++ src/components/EventList/EventList.js
@@ -1,104 +1,86 @@
 import React from "react";
 import { Link } from "gatsby";
-import { withPrefix } from "gatsby";
 
 const EventList = () => (
     <section className="EventList WhiteLabel">
+        <div className="EventList__TitleContainer WhiteLabel">
+            <h2 className="EventList__Title WhiteLabel">
+                Las Recientes
+            </h2>
+        </div>
         <div className="EventList__BoxContainer WhiteLabel">
-            <div className="EventList__TitleContainer WhiteLabel">
-                <h2 className="EventList__Title WhiteLabel">
-                    Las Recientes
-                </h2>
-            </div>
-            <div className="EventList__BlockContainer WhiteLabel">
-                <div className="EventList__TimeContainer WhiteLabel">
-                    <span className="EventList__Time WhiteLabel">
-                        12:00
-                    </span>
-                </div>
-                <div className="EventList__HeadlinesContainer WhiteLabel">
-                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
-                        <h3 className="EventList__HeadlinesText WhiteLabel">
-                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
-                        </h3>
-                    </Link>
-                </div>
-            </div>
-            <div className="EventList__BlockContainer WhiteLabel">
-                <div className="EventList__TimeContainer WhiteLabel">
-                    <span className="EventList__Time WhiteLabel">
-                        12:00
-                    </span>
-                </div>
-                <div className="EventList__HeadlinesContainer WhiteLabel">
-                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
-                        <h3 className="EventList__HeadlinesText WhiteLabel">
-                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
-                        </h3>
-                    </Link>
-                </div>
-            </div>
-            <div className="EventList__BlockContainer WhiteLabel">
-                <div className="EventList__TimeContainer WhiteLabel">
-                    <span className="EventList__Time WhiteLabel">
-                        12:00
-                    </span>
-                </div>
-                <div className="EventList__HeadlinesContainer WhiteLabel">
-                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
-                        <h3 className="EventList__HeadlinesText WhiteLabel">
-                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
-                        </h3>
-                    </Link>
-                </div>
-            </div>
-            <div className="EventList__BlockContainer WhiteLabel">
-                <div className="EventList__TimeContainer WhiteLabel">
-                    <span className="EventList__Time WhiteLabel">
-                        12:00
-                    </span>
-                </div>
-                <div className="EventList__HeadlinesContainer WhiteLabel">
-                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
-                        <h3 className="EventList__HeadlinesText WhiteLabel">
-                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
-                        </h3>
-                    </Link>
-                </div>
-            </div>
-            <div className="EventList__BlockContainer WhiteLabel">
-                <div className="EventList__TimeContainer WhiteLabel">
-                    <span className="EventList__Time WhiteLabel">
-                        12:00
-                    </span>
-                </div>
-                <div className="EventList__HeadlinesContainer WhiteLabel">
-                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
-                        <h3 className="EventList__HeadlinesText WhiteLabel">
-                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
-                        </h3>
-                    </Link>
-                </div>
-            </div>
-            <div className="EventList__BlockContainer WhiteLabel">
-                <div className="EventList__TimeContainer WhiteLabel">
-                    <span className="EventList__Time WhiteLabel">
-                        12:00
-                    </span>
-                </div>
-                <div className="EventList__HeadlinesContainer WhiteLabel">
-                    <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
-                        <h3 className="EventList__HeadlinesText WhiteLabel">
-                            Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
-                        </h3>
-                    </Link>
-                </div>
-            </div>
-            <div className="EventList__MoreContainer WhiteLabel">
-                <Link to="/" className="EventList__More WhiteLabel">
-                    Ver Más
+                <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                    <div className="EventList__TimeContainer WhiteLabel">
+                        <span className="EventList__Time WhiteLabel">
+                            12:00
+                        </span>
+                    </div>
+                    <h3 className="EventList__HeadlinesText WhiteLabel">
+                        Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                    </h3>
                 </Link>
-            </div>
+                <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                    <div className="EventList__TimeContainer WhiteLabel">
+                        <span className="EventList__Time WhiteLabel">
+                            12:00
+                        </span>
+                    </div>
+                    <h3 className="EventList__HeadlinesText WhiteLabel">
+                        Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                    </h3>
+                </Link>
+                <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                    <div className="EventList__TimeContainer WhiteLabel">
+                        <span className="EventList__Time WhiteLabel">
+                            12:00
+                        </span>
+                    </div>
+                    <h3 className="EventList__HeadlinesText WhiteLabel">
+                        Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                    </h3>
+                </Link>
+                <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                    <div className="EventList__TimeContainer WhiteLabel">
+                        <span className="EventList__Time WhiteLabel">
+                            12:00
+                        </span>
+                    </div>
+                    <h3 className="EventList__HeadlinesText WhiteLabel">
+                        Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                    </h3>
+                </Link>
+                <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                    <div className="EventList__TimeContainer WhiteLabel">
+                        <span className="EventList__Time WhiteLabel">
+                            12:00
+                        </span>
+                    </div>
+                    <h3 className="EventList__HeadlinesText WhiteLabel">
+                        Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                    </h3>
+                </Link>
+                <Link to="/" className="EventList__HeadlinesContainer WhiteLabel">
+                    <div className="EventList__TimeContainer WhiteLabel">
+                        <span className="EventList__Time WhiteLabel">
+                            12:00
+                        </span>
+                    </div>
+                    <h3 className="EventList__HeadlinesText WhiteLabel">
+                        Mandzukic marca el gol de la ventaja en tiempo extra, Francia-Croacia Final Rusia
+                    </h3>
+                </Link>
+                <Link to="/" className="EventList__MoreContainer WhiteLabel">
+                    <span className="EventList__More WhiteLabel">
+                        Ver Más
+                    </span>
+                    <div className="EventList__MoreIconContainer WhiteLabel">
+                        <div className="EventList__MoreIcon Icn_ArrowRight WhiteLabel">
+                            <span className="Display">
+                                Icn_ArrowRight
+                            </span>
+                        </div>
+                    </div>
+                </Link>
         </div>
     </section>
 );
