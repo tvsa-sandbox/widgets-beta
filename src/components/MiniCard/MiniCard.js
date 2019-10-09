411185ee447b624985ceeaa32faf31b70ee7236f
Karen Torres <karen.torres@televisatim.com>
2019-10-09 18:41:46 -0500

    FIX: Images Fix

--- src/components/MiniCard/MiniCard.js
+++ src/components/MiniCard/MiniCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "gatsby-link";
+import { withPrefix } from "gatsby";
 
 const MiniCard = () => (
     <section className="MiniCard WhiteLabel">
@@ -7,10 +8,10 @@ const MiniCard = () => (
             <Link to="/" className="MiniCard__Item WhiteLabel">
                 <div className="MiniCard__ItemImageContainer WhiteLabel">
                     <picture>
-                        <source media="(min-width: 0px) and (max-width: 414px)" srcSet="./static/aspect-ratio-images/16x9-Graphic1-209x114.jpg" />
-                        <source media="(min-width: 415px) and (max-width: 768px)" srcSet="./static/aspect-ratio-images/16x9-Graphic1-258x145.jpg" />
+                        <source media="(min-width: 0px) and (max-width: 414px)"  srcSet={withPrefix("./static/aspect-ratio-images/16x9-Graphic1-209x114.jpg")} />
+                        <source media="(min-width: 415px) and (max-width: 768px)" srcSet={withPrefix("./static/aspect-ratio-images/16x9-Graphic1-258x145.jpg")} />
                         <source media="(min-width: 769px)" srcSet="./static/aspect-ratio-images/16x9-Graphic1-258x145.jpg" />
-                        <img className="MiniCard__ItemImage WhiteLabel" src="./static/aspect-ratio-images/16x9-Graphic1-209x114.jpg" alt="Test" />
+                        <img className="MiniCard__ItemImage WhiteLabel" src={withPrefix("./static/aspect-ratio-images/16x9-Graphic1-209x114.jpg")} alt="Test" />
                     </picture>
                 </div>
                 <div className="MiniCard__ItemCaption WhiteLabel">
@@ -24,10 +25,10 @@ const MiniCard = () => (
             <Link to="/" className="MiniCard__Item WhiteLabel">
                 <div className="MiniCard__ItemImageContainer WhiteLabel">
                     <picture>
-                        <source media="(min-width: 0px) and (max-width: 414px)" srcSet="./static/aspect-ratio-images/16x9-Graphic1-209x114.jpg" />
-                        <source media="(min-width: 415px) and (max-width: 768px)" srcSet="./static/aspect-ratio-images/16x9-Graphic1-258x145.jpg" />
+                        <source media="(min-width: 0px) and (max-width: 414px)"  srcSet={withPrefix("./static/aspect-ratio-images/16x9-Graphic1-209x114.jpg")} />
+                        <source media="(min-width: 415px) and (max-width: 768px)" srcSet={withPrefix("./static/aspect-ratio-images/16x9-Graphic1-258x145.jpg")} />
                         <source media="(min-width: 769px)" srcSet="./static/aspect-ratio-images/16x9-Graphic1-258x145.jpg" />
-                        <img className="MiniCard__ItemImage WhiteLabel" src="./static/aspect-ratio-images/16x9-Graphic1-209x114.jpg" alt="Test" />
+                        <img className="MiniCard__ItemImage WhiteLabel" src={withPrefix("./static/aspect-ratio-images/16x9-Graphic1-209x114.jpg")} alt="Test" />
                     </picture>
                 </div>
                 <div className="MiniCard__ItemCaption WhiteLabel">
