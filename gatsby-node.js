a0341c465f1902b5f4719d8e2d72a725d818fd11
jose morales <jose.morales@televisatim.com>
2019-10-24 16:25:01 -0500

    ADD gzip test

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,37 @@
  */
 
 // You can delete this file if you're not using it
+
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+// const iltorb = require('iltorb');
+const glob = require('glob');
+const ImageminWebP = require('imagemin-webp');
+// const ImageminPlugin = require('imagemin-webpack-plugin').default;
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+exports.onPostBuild = () =>
+  new Promise((resolve, reject) => {
+    try {
+      const publicPath = path.join(__dirname, 'public');
+      const options = { nodir: true };
+      const gzippable = glob.sync(`${publicPath}/**/?(*.html|*.js|*.css|*.svg)`, options);
+      gzippable.forEach((file) => {
+        const content = fs.readFileSync(file);
+        const zipped = zlib.gzipSync(content);
+        fs.writeFileSync(`${file}.gz`, zipped);
+
+        // const brotlied = iltorb.compressSync(content);
+        // fs.writeFileSync(`${file}.br`, brotlied);
+        imagemin([`${publicPath}/static/*.{jpg,png}`], '`${publicPath}/static/aspect', {
+		use: [
+			imageminWebp({quality: 50})
+		]
+      });
+    } catch (e) {
+      reject(new Error('onPostBuild: Could not compress the files'));
+    }
+
+    resolve();
+  });
