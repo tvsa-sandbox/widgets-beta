8a504e8d5d4f0795275d9c8686fe1b89791fd837
Isaac Ortiz <isaac.ortiz@televisatim.com>
2019-10-28 16:22:01 -0600

    ADD: Webp package

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,7 @@
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
+ * imagemin-webp docs: https://github.com/imagemin/imagemin-webp
  */
 
 // You can delete this file if you're not using it
