01fdfda63abde568af8697a55e3fb56dc404f89d
Isaac Ortiz <isaac.ortiz@televisatim.com>
2019-10-28 16:13:31 -0600

    ADD: Webp package

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,11 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
 const fs = require("fs");
 const path = require("path");
 const zlib = require("zlib");
