0502e0a336d090575ce219174bae2dea4c4642e7
Isaac Ortiz <isaac.ortiz@televisatim.com>
2019-10-28 16:03:45 -0600

    ADD: Webp package

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,41 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const fs = require("fs");
+const path = require("path");
+const zlib = require("zlib");
+const glob = require("glob");
+const ImageminWebP = require("imagemin-webp");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-// You can delete this file if you're not using it
-
-const fs = require('fs');
-const path = require('path');
-const zlib = require('zlib');
-// const iltorb = require('iltorb');
-const glob = require('glob');
-const ImageminWebP = require('imagemin-webp');
-// const ImageminPlugin = require('imagemin-webpack-plugin').default;
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-exports.onPostBuild = () =>
-  new Promise((resolve, reject) => {
+exports.onPostBuild = () => new Promise((resolve, reject) => {
     try {
-      const publicPath = path.join(__dirname, 'public');
-      const options = { nodir: true };
-      const gzippable = glob.sync(`${publicPath}/**/?(*.html|*.js|*.css|*.svg)`, options);
-      gzippable.forEach((file) => {
-        const content = fs.readFileSync(file);
-        const zipped = zlib.gzipSync(content);
-        fs.writeFileSync(`${file}.gz`, zipped);
-
-        // const brotlied = iltorb.compressSync(content);
-        // fs.writeFileSync(`${file}.br`, brotlied);
-        imagemin([`${publicPath}/static/*.{jpg,png}`], '`${publicPath}/static/aspect', {
-		use: [
-			imageminWebp({quality: 50})
-		]
-      });
+        const publicPath = path.join(__dirname, "public");
+        const options = { nodir: true };
+        const gzippable = glob.sync(`${publicPath}/**/?(*.html|*.js|*.css|*.svg)`, options);
+        gzippable.forEach((file) => {
+            const content = fs.readFileSync(file);
+            const zipped = zlib.gzipSync(content);
+            fs.writeFileSync(`${file}.gz`, zipped);
+        });
     } catch (e) {
-      reject(new Error('onPostBuild: Could not compress the files'));
-    }
+        reject(new Error("onPostBuild: Could not compress the files"));
+    } resolve();
+});
 
-    resolve();
-  });
+exports.onCreateWebpackConfig = ({
+    actions,
+}) => {
+    actions.setWebpackConfig({
+        plugins: [
+            new CopyWebpackPlugin([{
+                from: "./public/static/aspect-ratio-images/**/**",
+                to: "./static/aspect-ratio-images/webp/[name].webp",
+            }]),
+            new ImageminPlugin({
+                plugins: [ImageminWebP({ quality: 50 })],
+            }),
+        ],
+    });
+};
