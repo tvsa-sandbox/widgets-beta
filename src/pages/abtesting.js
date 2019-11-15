0f35cef1a5c0be874ee7efa1045cc6bd6f9be785
edgarvj <edgar.villeda@televisatim.com>
2019-11-14 19:02:58 -0600

    FIX: Delete Boton

--- src/pages/abtesting.js
+++ src/pages/abtesting.js
@@ -24,7 +24,6 @@ class App extends React.Component {
             <Related2 />
           </Variant>
         </Experiment>
-        // <button onClick={this.onButtonClick}>Boton</button>
       </div>
     );
   }
