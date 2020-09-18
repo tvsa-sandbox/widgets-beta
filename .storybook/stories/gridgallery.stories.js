6711941632096d6dc4d401403bd5b199b4fbe16c
jose morales <jose.morales@televisatim.com>
2020-09-17 22:20:56 -0500

    FIX: Colors

--- .storybook/stories/gridgallery.stories.js
+++ .storybook/stories/gridgallery.stories.js
@@ -18,14 +18,34 @@ export const GRIDGALLERY = args => {
             query: "(min-width: 0px)",
             size: bsp,
         },
+        {
+            aspect: "aspect4x3",
+            query: "(min-width: 0px)",
+            size: "medium",
+        },
+        {
+            aspect: "aspect3x4",
+            query: "(min-width: 0px) and (max-width: 414px)",
+            size: "mobile-large",
+        },
     ];
     const IMG = Utils.getSources(Mock.renditions, SOURCES);
     const SRC = {
-        desktop: {},
-        mobile: IMG[0],
+        gridgallery: [
+            {
+                desktop: {},
+                mobile: IMG[0],
+            },
+        ],
+        gridgal: [
+            {
+                desktop: IMG[1],
+                mobile: IMG[2],
+            },
+        ],
     };
     const props = () => ({
         title: text("Titulo", "Titulo de test"),
     });
-    return <GridGallery ratio={aspect} src={SRC} {...props} />;
+    return <GridGallery ratio={aspect} src={SRC.gridgallery[0]} gal={SRC.gridgal[0]} {...props} />;
 };
