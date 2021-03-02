b3ee8eb64fb992ecee84c2731080323383025537
jose morales <crozzager@yahoo.com.mx>
2021-03-02 11:59:36 -0600

    Fix: styles

---
+++ .storybook/stories/Versus.stories.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Utils from "@televisadigital/nxtv-utilities";
+import { Title } from "accessories";
+
+import Versus from "../../lib/widgets/Versus";
+
+import Mock from "./imageMock.json";
+
+export default {
+    title: "Desing System/TUDN/Versus",
+    component: Versus,
+};
+
+export const VERSUS = args => {
+    const { aspect = "aspect1x1", bsp = "thumbnail" } = args;
+    const SOURCES = [
+        {
+            aspect,
+            query: "(min-width: 0px)",
+            size: bsp,
+        },
+    ];
+    const IMG = Utils.getSources(Mock.renditions, SOURCES);
+    const SRC = {
+        desktop: {},
+        mobile: IMG[0],
+    };
+    return (
+        <div>
+            <Title variant="h2">Versus</Title>
+            <Versus ratio={aspect} src={SRC} />
+        </div>
+    );
+};
